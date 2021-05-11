344967a718a8bb628aed9a27f1bc98ad0fa8ec7f
gluemchen <gluemchen81@googlemail.com>
2021-05-11 19:44:12 +0200

    Made a programm in c  that prints the assembler function as a byte

--- MASM Test/main.cpp
+++ MASM Test/main.cpp
@@ -2,11 +2,21 @@
 
 using namespace std;
 
-extern "C" int someFunc();
-extern "C" int testReg();
+void PrintBinary(int i)
+{
+	for (int s = 15; s >= 0; s--)
+		cout << ((i >> s) & 1);
+	cout << endl;
+}
+// Function Declaration of TestFunction() for usage in asm
+extern "C" int TestFunction();
 
-int main() {
-	cout << "The result of someFunc() is: " << someFunc() << endl;
-	cout << "The result of testReg() is: " << testReg() << endl;
+// Programm calls 
+int main() 
+{
+	PrintBinary(TestFunction());
+
+	cin.get();
+
 	return 0;
 };
